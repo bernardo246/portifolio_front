9d837224890f927e81a6e6ebb666fdff65a01746
Bernardo Cavalcanti Carneiro Leão <bernardocavalcanticarneiroleao@MacBook-Air-de-Bernardo.local>
2026-08-10 16:03:12 -0300

    add access to ['192.168.2.52']

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  allowedDevOrigins: ['192.168.2.52'],
   images: {
     formats: ["image/avif", "image/webp"]
   }
